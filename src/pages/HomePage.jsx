14221afc7f053354b47acd895c45fd7d76ee47cd
teymur54 <132904290+teymur54@users.noreply.github.com>
2023-12-18 11:29:20 +0400

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -43,7 +43,7 @@ const HomePage = () => {
 
   const handleSearchInputChange = (e) => {
     const value = e.target.value;
-    const sanitizedValue = value.replace(/[^\w\d]/g, "");
+    const sanitizedValue = value.replace(/[^\w\dƏəХхЪъЖжЮюБб]/g, "");
     setSearch(sanitizedValue);
   };
 
